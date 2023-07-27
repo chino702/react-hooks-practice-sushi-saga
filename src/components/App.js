10504798a5359e6add2692abc3c59fb477989a3f
chino702 <chinox3702@gmail.com>
2023-07-27 04:24:55 -0700

    finished the components for the sushi saga practice

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([]);
+  const [budget, setBudget] = useState(100);
+
+  useEffect(() => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => setSushis(data));
+  }, []);
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushis} budget={budget} setBudget={setBudget} />
+      <Table plates={[]} budget={budget} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import Sushi from "./Sushi";
 import MoreButton from "./MoreButton";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, budget, setBudget }) {
+  const [startIndex, setStartIndex] = useState(0);
+
+  const handleEatSushi = (sushi) => {
+    if (sushi.price <= budget) {
+      setBudget((prevBudget) => prevBudget - sushi.price);
+      // Add the sushi to the plates or do other logic here
+    } else {
+      // Handle insufficient budget scenario
+      alert("You don't have enough budget to eat this sushi!");
+    }
+  };
+
+  const renderSushis = sushis.slice(startIndex, startIndex + 4).map((sushi) => (
+    <Sushi key={sushi.id} sushi={sushi} onEatSushi={handleEatSushi} />
+  ));
+
+  const handleMoreClick = () => {
+    setStartIndex((prevIndex) => (prevIndex + 4) % sushis.length);
+  };
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {renderSushis}
+      <MoreButton onClick={handleMoreClick} />
     </div>
   );
 }
